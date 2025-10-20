26b7493bc185d7ac1d03fc578f309d3e150d7e60
Ikechukwu001 <michaeljustice727@gmail.com>
2025-10-20 11:34:53 +0100

    fixed the button bug

--- Components/AuthSection.jsx
+++ Components/AuthSection.jsx
@@ -2,7 +2,7 @@
 
 import { motion } from "framer-motion";
 import { useState } from "react";
-import { Button } from "@/components/ui/button";
+import { Button } from "@/Components/ui/button";
 import { Mail, Lock, User } from "lucide-react";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
